f7a7ae273e59e79d86ab60c9a6163177982b7c6e
abhijoshi23 <avjoshi1@myseneca.ca>
2022-07-20 14:45:55 -0400

    first commit

---
+++ src/app/artist-discography/artist-discography.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import albumData from '../data/Albums.json';
+import artistData from '../data/Artist.json';
+
+@Component({
+  selector: 'app-artist-discography',
+  templateUrl: './artist-discography.component.html',
+  styleUrls: ['./artist-discography.component.css']
+})
+export class ArtistDiscographyComponent implements OnInit {
+  albums: any;
+  artist: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.albums = albumData.items.filter(
+      (curValue, index, self) =>
+        self.findIndex(
+          (t) => t.name.toUpperCase() === curValue.name.toUpperCase()
+        ) === index
+    );
+
+    this.artist = artistData;
+  }
+
+}
