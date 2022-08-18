447658680119fd3cdf8b9f395dc63e3142cc4b5e
abhijoshi23 <avjoshi1@myseneca.ca>
2022-08-17 20:01:57 -0400

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   clientID: 'af6edfe218ec41b1820f47e9d69f84fd',
   clientSecret: 'd0b3ab5d34524440b84ddfd16177526d',
+  userAPIBase: 'https://spotifyuiapi.herokuapp.com/user',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: true,
   clientID: 'af6edfe218ec41b1820f47e9d69f84fd',
   clientSecret: 'd0b3ab5d34524440b84ddfd16177526d',
+  userAPIBase: 'https://spotifyuiapi.herokuapp.com/user',
 };
 /*
  * For easier debugging in development mode, you can import the following file
